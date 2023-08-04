a5f1067909132f351e2e6449ae28e751370f4cc2
dev-andalib <iftakherandalib@gmail.com>
2023-08-04 21:33:43 +0600

    prob with resize and load

--- js/script.js
+++ js/script.js
@@ -8,6 +8,17 @@ if (vw < 800) {
   $('.nav_menu').hide();
 }
 
+$(window).on('load', function() {
+  vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+  if (vw < 800) {
+    $('.nav_menu').hide();
+  }else{
+    $('.nav_menu').show();
+  }
+});
+
+
 $(window).on('resize', function() {
   vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
